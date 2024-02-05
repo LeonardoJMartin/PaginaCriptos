ff2464c845d3fcc19951375d4012eedcf592fe76
Leonardo Martin <leonardo.martin.95@hotmail.com>
2024-02-04 22:34:10 -0300

    iniciado estrutura html, e alterado a lógica do js para extrair dados fornecidos nos inputs

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 var listaTodasCriptos = [];
 
-function adicionarCompra(nome){
+$('#clicarAddTransacao').click(function(event) {
+    desocultar('#adicionarTransacao');
+});
+$('#clicarIncluir').click(controlaAdicao);
+
+function controlaAdicao(){
+    adicionarCompra();
+    resetarCampos();
+    mostrarTransacoesInclusas();
+}
+
+function adicionarCompra(){
     let operacao = {
-        dataCompra: "17/12/1995",
-        valorPago: 19000,
-        valorMoeda: 40000,              
+        dataCompra: $('#dataCompra').val(),
+        valorPago: $('#valorPago').val(),
+        precoMoeda: $('#precoMoeda').val(),
+        nomeMoeda: $('#nomeMoeda').val(),              
     }
-    operacao.quantidadeMoeda = operacao.valorPago / operacao.valorMoeda;   
+    operacao.quantidadeMoeda = operacao.valorPago / operacao.precoMoeda;   
     
-    let verificaSeJaExiste = listaTodasCriptos.find(objeto => objeto.nome === nome); // Verifica se existe uma cripto com esse nome armazenada
+    let verificaSeJaExiste = listaTodasCriptos.find(objeto => objeto.nome === nomeMoeda); // Verifica se existe uma cripto com esse nome armazenada
 
     if(verificaSeJaExiste){  //Se ja existe, adiciona uma nova operação e acrescenta o valor obtido
         verificaSeJaExiste.listaOperacaoCripto.push(operacao);
@@ -17,10 +29,10 @@ function adicionarCompra(nome){
     }
     else{ //Se não existe, cria um novo objeto para essa cripto, e adiciona na listaTodasCriptos
         let cripto = {
-            nome: nome, 
+            nome: operacao.nomeMoeda, 
             listaOperacaoCripto: [operacao], 
             totalObtido: operacao.quantidadeMoeda, 
-            mediaTotalValor: operacao.valorMoeda
+            mediaTotalValor: operacao.precoMoeda
         };
         calculaMediaCripto(cripto);
         listaTodasCriptos.push(cripto);
@@ -31,7 +43,35 @@ function adicionarCompra(nome){
 function calculaMediaCripto(criptoAtual){ // Calcula preço médio da moeda
     let totalTransacao = 0;
     for(let transacao of criptoAtual.listaOperacaoCripto){
-        totalTransacao += transacao.valorMoeda * transacao.quantidadeMoeda;
+        totalTransacao += transacao.precoMoeda * transacao.quantidadeMoeda;
     }
     criptoAtual.mediaTotalValor = totalTransacao / criptoAtual.totalObtido;
+}
+
+function resetarCampos(){
+    $('#dataCompra').val('');
+    $('#valorPago').val('');
+    $('#precoMoeda').val(''); 
+    $('#nomeMoeda').val('');
+}
+
+function ocultar(val){
+    $(val).addClass('ocultar');
+}
+
+function desocultar(val){
+    $(val).removeClass('ocultar');
+}
+
+function mostrarTransacoesInclusas(){
+    $('#listaTrasacoes').empty();
+    for (let moeda of listaTodasCriptos) {
+        let clonaBloco = $('#listaTrasacoes').clone();
+        clonaBloco.append('<div>Moeda: ' + moeda.nome + '</div>');
+        clonaBloco.append('<div>Média total: ' + moeda.mediaTotalValor + '</div>');
+        clonaBloco.append('<div>Quantidade de ' + moeda.nome +' total: ' + moeda.totalObtido + '</div>');
+        clonaBloco.append();
+        clonaBloco.append('<div>Número de transações: ' + moeda.listaOperacaoCripto.length + '</div>');
+        clonaBloco.appendTo('#listaTrasacoes');
+    }
 }
